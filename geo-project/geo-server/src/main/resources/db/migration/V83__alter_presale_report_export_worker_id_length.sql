0ef6aa0369dfb4179e4a3b8689152c8cebc9f8f2
wangheng <851023374@qq.com>
2026-04-25 11:52:19 +0800

    fix: clarify presale export worker id migration

---
+++ geo-project/geo-server/src/main/resources/db/migration/V83__alter_presale_report_export_worker_id_length.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE presale_report_export
+    MODIFY COLUMN worker_id VARCHAR(128) NULL
+    COMMENT '抢占任务的 worker 标识: host前16位:pid:uuid前8位';
--- geo-project/geo-server/src/main/resources/db/migration/V83__widen_presale_export_worker_id.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE presale_report_export
-    MODIFY COLUMN worker_id VARCHAR(128) NULL COMMENT '抢占任务的 worker 标识';
