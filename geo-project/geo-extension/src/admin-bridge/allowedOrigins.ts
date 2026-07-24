4273b4ab05de18c151b0b3d4fa69f6a94d9d20da
wangheng <851023374@qq.com>
2026-07-24 12:02:05 +0800

    chore(extension): remove unused legacy browser extension

--- geo-project/geo-extension/src/admin-bridge/allowedOrigins.ts
+++
@@ -1,6 +0,0 @@
-const EXTENSION_PROFILE = __EXTENSION_PROFILE__
-const ALLOWED_ADMIN_ORIGINS = EXTENSION_PROFILE.adminOrigins.map(origin => origin.replace(/\/$/, ''))
-
-export function isAllowedAdminOrigin(origin: string): boolean {
-  return ALLOWED_ADMIN_ORIGINS.includes(origin)
-}
